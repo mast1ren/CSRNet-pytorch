d16fb17b7b65e7384d0127a071ca1447d9a169af
masteren <masterenlu@outlook.com>
2022-10-05 12:06:16 +0800

    syn server code

--- dataset.py
+++ dataset.py
@@ -5,14 +5,18 @@ import numpy as np
 from torch.utils.data import Dataset
 from PIL import Image
 from image import *
-import torchvision.transforms.functional as F
+import matplotlib.pyplot as plt
+# import cv2
+# import torchvision.transforms.functional as F
+from torchvision import datasets, transforms
+
 
 class listDataset(Dataset):
-    def __init__(self, root, shape=None, shuffle=True, transform=None,  train=False, seen=0, batch_size=1, num_workers=4):
+    def __init__(self, root, shape=None, shuffle=True, transform=None,  train=False, seen=0, batch_size=1, num_workers=4, downsample=4):
         if train:
-            root = root *4
+            root = root * 4
         random.shuffle(root)
-        
+
         self.nSamples = len(root)
         self.lines = root
         self.transform = transform
@@ -21,26 +25,36 @@ class listDataset(Dataset):
         self.seen = seen
         self.batch_size = batch_size
         self.num_workers = num_workers
-        
-        
+        self.downsample = downsample
+
     def __len__(self):
         return self.nSamples
+
     def __getitem__(self, index):
-        assert index <= len(self), 'index range error' 
-        
+        assert index <= len(self), 'index range error'
+
         img_path = self.lines[index]
-        
-        img,target = load_data(img_path,self.train)
-        
-        #img = 255.0 * F.to_tensor(img)
-        
-        #img[0,:,:]=img[0,:,:]-92.8207477031
-        #img[1,:,:]=img[1,:,:]-95.2757037428
-        #img[2,:,:]=img[2,:,:]-104.877445883
 
-
-        
-        
+        img, target = load_data(img_path, self.train)
         if self.transform is not None:
             img = self.transform(img)
-        return img,target
+
+        return img, target
+
+if __name__ == '__main__':
+    train_list = ["E:/dronebird/dronebird/train/DJI_0014/data/000000.jpg"]
+    train_loader = torch.utils.data.DataLoader(
+        listDataset(train_list, shuffle=True,
+                            transform=transforms.Compose([
+                                transforms.ToTensor(), transforms.Normalize(
+                                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                            ]), train=True, seen=0, batch_size=1, num_workers=4),
+        batch_size=1)
+
+    for i, (img, target) in enumerate(train_loader):
+        plt.figure(1)
+        plt.imshow(img.squeeze(0).permute(1, 2, 0))
+        # plt.imshow(img.permute(1, 2, 0))
+        plt.figure(2)
+        plt.imshow(target.permute(1, 2, 0))
+        plt.show()
---
+++ make_json.py
@@ -0,0 +1,28 @@
+import os
+import json
+
+
+def get_jpg_path(path, set):
+    jpg_path = []
+    
+    for root, dirs, files in os.walk(os.path.join(path, set)):
+        c = 0
+        tmp_path = []
+        for file in files:
+            if file.endswith('.jpg'):
+                tmp_path.append(os.path.join(root, file))
+        tmp_path.sort()
+        for path in tmp_path:
+            if c % 5 == 0:
+                jpg_path.append(path)
+            c += 1
+
+    print(len(jpg_path))
+    
+    jpg_path_json = json.dumps(jpg_path)
+    with open(set + '.json', 'w') as f:
+        f.write(jpg_path_json)
+    f.close()
+
+for var in ['train', 'test', 'val']:
+    get_jpg_path('../ds/dronebird', var)
